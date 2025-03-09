7e408408d72d6f2d2f10721e4bf5be040e46574c
0xkenn <kennycharlest.gmail.com>
2025-03-09 23:24:05 +0800

    responsive svgs

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SoftText from '../fonts/soft-text'
 import BlurFade from '../magicui/blur-fade'
-import LanguageList from '../languageList'
+import {DbList, LanguageList} from '../languageList'
 
 
 const About = () => {
@@ -19,32 +19,32 @@ const About = () => {
         </SoftText>
        </BlurFade>
         </div>
-        <div className="flex flex-col px-4">
-  {/* Programming Languages Section */}
-  <div className="flex flex-row">
-    <div className="w-1/4">
-      <p>Programming Languages</p>
-    </div>
-    <div className="w-2/5 ml-10 grid grid-cols-5 gap-4">
-      <LanguageList />
-    </div>
-  </div>
 
-  {/* Database Section */}
-  <div className="flex flex-row mt-6">
-    <div className="w-1/4">
-      <p>Databases</p>
-    </div>
-    <div className="w-2/5 ml-10 grid grid-cols-5 gap-4">
-      {/* <DatabaseList /> */}
-    </div>
-  </div>
-</div>
-
-
-
-        {/* end */}
+  <div className="flex flex-col px-12 gap-y-16">
+    {/* languages */}
+    <div className="flex flex-row">
+      <div className="w-1/4">
+        <p className='text-sm md:text-md lg:text-lg'>Programming Languages</p>
       </div>
+      <div className="w-2/5 ml-10 grid grid-cols-5 gap-4">
+        <LanguageList />
+      </div>
+    </div>
+    {/* database */}
+    <div className="flex flex-row mt-6">
+      <div className="w-1/4">
+        <p>Databases</p>
+      </div>
+      <div className="w-2/5 ml-10 grid grid-cols-5 gap-4">
+        <DbList/>
+      </div>
+      
+    </div>
+    
+  </div>
+
+  </div>
+  <DbList/>
     </section>
   )
 }
--- src/components/languageList.jsx
+++ src/components/languageList.jsx
@@ -1,17 +1,32 @@
-import { Dart, Javascript, Php, Python, Typescript } from "@/config/site";
+import { Dart, Javascript, Php, Postgre, Python, Typescript } from "@/config/site";
 import React from "react";
 
 const languages = [<Python />, <Javascript />, <Php/>, <Dart/>, <Typescript/>];
+const databases = [<Postgre/>];
 
 
-export default function LanguageList() {
-  console.log(languages);
+ const LanguageList = () => {
   return (
     <>
       {languages.map((component, index) => (
-        <div key={index} className="mx-4"> {component} </div>
-      ))}
+       <div key={index} className="w-6 sm:w-8 sm:h-8 md:w-12 md:h-12 mx-4 md:mx-4">
+       {component}
+     </div>
+        ))}
       
     </>
   );
 }
+
+const DbList = () => {
+  
+  return (
+    <>
+    {databases.map((component, index) => {
+      <div key={index} className="mx-4 h-4 w-4"> {component} </div>
+    })}
+    </>
+  )
+}
+
+export { LanguageList, DbList };
